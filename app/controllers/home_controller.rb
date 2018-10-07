69c98f252cbb385f293a5898514069199fb0f05f
Tung Lam <coldwind1911@gmail.com>
2018-10-07 09:17:45 +0000

    address & review post detail + fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @place_reviews = Place.paginate(page: params[:page], per_page: 10)
+    @place_reviews = Place.paginate(page: params[:page], per_page: 5)
     @popular_address = Address.joins(:places).group("places.address_id").order("count(places.address_id) desc").take 10
     @recent_reviews = Place.order("created_at desc").take 3
     @most_viewed = Place.order("views_number desc").take 5
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,15 @@
-class PlaceController < ApplicationController
+class PlacesController < ApplicationController
+    before_action :set_place, only: [:show]
+    
     def show
+        @popular_address = Address.joins(:places).group("places.address_id").order("count(places.address_id) desc").take 10
+        @related_reviews = Place.where(address_id: @place.address.id).take 4
+        @same_author_reviews = Place.where(user_id: @place.user.id).take 4
     end
+    
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_place
+          @place = Place.find(params[:id])
+        end
 end
