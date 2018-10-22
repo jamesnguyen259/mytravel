d3c44f9c97989761d130341506400e029dde98bb
Hung Pham Hoang <hungph.dev.ict@gmail.com>
2018-10-22 15:12:27 +0000

    about page & fix display

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get '/style', to: 'home#style', as: 'style'
-  get '/fashion', to: 'home#fashion', as: 'fashion'
 
   resources :users
   resources :places
