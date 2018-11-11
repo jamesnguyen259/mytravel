9ded615931cd8cf2c58d024236dac61e8c2753dc
Tung Lam <coldwind1911@gmail.com>
2018-11-11 08:31:04 +0000

    Add mailer for confirmation and forgot password

---
+++ db/migrate/20181111052216_add_confirmation_send_at_to_users.rb
@@ -0,0 +1,5 @@
+class AddConfirmationSendAtToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :confirmation_sent_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181111025219) do
+ActiveRecord::Schema.define(version: 20181111054508) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -77,6 +77,9 @@ ActiveRecord::Schema.define(version: 20181111025219) do
     t.integer  "avatar_file_size",       limit: 8
     t.datetime "avatar_updated_at"
     t.boolean  "is_admin",                         default: false
+    t.datetime "confirmation_sent_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
