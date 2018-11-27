5d347a026fb09027cb1bf2a6081b3d920e166f6c
Tung Lam <coldwind1911@gmail.com>
2018-11-27 17:05:20 +0000

    Fb log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable,
-           :confirmable, :trackable
+           :confirmable, :trackable, 
+           :omniauthable, :omniauth_providers => [:facebook]
     has_many :places
     has_many :comments
     validates :name, presence: true
@@ -15,4 +16,21 @@ class User < ActiveRecord::Base
         
     validates_attachment_content_type :avatar, 
         :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+        
+    def self.new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
+    end
+
+    def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.name = auth.info.name
+        user.skip_confirmation!
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181111054508) do
+ActiveRecord::Schema.define(version: 20181127165303) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 20181111054508) do
     t.datetime "confirmation_sent_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
